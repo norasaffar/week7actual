1d4c18e16e65c28342ef08d86df02b0cc97726d2
NoraSaffar <norasaffar8910@gmail.com>
2023-12-11 14:29:50 +0000

    removed static server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 
 const app = express();
-// http://localhost:5001/example
-app.use("/Studioghibli", express.static("Studioghibli"));
+
 
 app.listen(5001, () =>{
     console.log("server is listening on port 5001");
